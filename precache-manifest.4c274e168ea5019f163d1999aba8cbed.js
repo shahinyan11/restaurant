016ba8a8bbb12b37accee35c3bb8ce67f63fcc18
Shahe <shahe.shahinyan@gmail.com>
2019-05-04 18:04:18 +0400

    Updates

---
+++ precache-manifest.4c274e168ea5019f163d1999aba8cbed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ca229b22caf8528f9b5",
+    "url": "/static/js/main.2ca229b2.chunk.js"
+  },
+  {
+    "revision": "ad18217d177a66d8de66",
+    "url": "/static/js/1.ad18217d.chunk.js"
+  },
+  {
+    "revision": "2ca229b22caf8528f9b5",
+    "url": "/static/css/main.8686a531.chunk.css"
+  },
+  {
+    "revision": "322c96d393f9b222b780a37948954ca7",
+    "url": "/index.html"
+  }
+];
